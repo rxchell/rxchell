2c50f6a3419574dca7519922d55172d464ea87e7
rxchell <e0969575@u.nus.edu>
2024-01-09 12:49:30 +0800

    update live date

--- index.js
+++ index.js
@@ -16,28 +16,12 @@ let DATA = {
   }),
 };
 
-async function setWeatherInformation() {
-  await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=Singapore&appid=${process.env.e2cde8e54d9f77964843492c4b881742}}&units=metric`
-  )
-    .then(r => r.json())
-    .then(r => {
-      DATA.temperature = Math.round(r.main.temp);
-      DATA.weather = r.weather[0].description;
-    });
-}
-
 async function generateReadMe() {
   await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
   });
-}
-
-async function action() {
-  await setWeatherInformation();   
-  await generateReadMe();
 }
 
 action();
